d47c1f899f48b3fad5cb16704bd616e836c3c89e
Tobey02 <tobey.lim@student.unsw.edu.au>
2023-03-01 16:50:37 +1100

    Test

--- app/src/main/java/com/example/infs3605groupproject/MainActivity.java
+++ app/src/main/java/com/example/infs3605groupproject/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Hey, I just made this file, this is a test
     }
 }
