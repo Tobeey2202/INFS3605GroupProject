23a3a99acab5829c83fc0d3923024adb4e5d1ebe
Tobey02 <tobey.lim@student.unsw.edu.au>
2023-03-16 01:47:48 +1100

    Fragment works for navigation now - added to the plant detail page

--- app/src/main/java/com/example/infs3605groupproject/Fragments/BlankFragment.java
+++ app/src/main/java/com/example/infs3605groupproject/Fragments/BlankFragment.java
@@ -1,5 +1,6 @@
 package com.example.infs3605groupproject.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.example.infs3605groupproject.GoogleMapsActivity;
+import com.example.infs3605groupproject.MainActivity;
+import com.example.infs3605groupproject.MapActivity;
+import com.example.infs3605groupproject.ProfileActivity;
+import com.example.infs3605groupproject.QrScannerActivity;
 import com.example.infs3605groupproject.R;
 
 /**
@@ -21,6 +29,7 @@ public class BlankFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private ImageButton homeButton, mapButton, codeButton, profileButton;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,6 +70,60 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View view =  inflater.inflate(R.layout.fragment_blank, container, false);
+        homeButton = view.findViewById(R.id.detailHomeButton2);
+
+        mapButton = view.findViewById(R.id.detailMapButton2);
+
+        codeButton = view.findViewById(R.id.detailCodeButton2);
+
+        profileButton = view.findViewById(R.id.detailProfileButton2);
+
+
+
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {clickedOnHomePage();}
+        });
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {clickedOnMapPage();}
+        });
+
+        codeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {clickedOnCodePage();}
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {clickedOnProfilePage();}
+        });
+
+
+        return view;
+    }
+
+    public void clickedOnHomePage(){
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickedOnMapPage(){
+        Intent intent = new Intent(getActivity(), GoogleMapsActivity.class);
+        startActivity(intent);
+        System.out.print("map page works");
+    }
+
+    public void clickedOnCodePage(){
+        Intent intent = new Intent(getActivity(), QrScannerActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickedOnProfilePage(){
+        Intent intent = new Intent(getActivity(), ProfileActivity.class);
+        startActivity(intent);
     }
 }
