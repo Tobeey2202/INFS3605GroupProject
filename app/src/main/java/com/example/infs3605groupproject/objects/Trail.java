9a537f3c9c26382dd05405ba9da9c8e58b6db31c
Nessa <z5366549@ad.unsw.edu.au>
2023-03-29 12:46:13 +1100

    fix ui for acknowledge

--- app/src/main/java/com/example/infs3605groupproject/objects/Trail.java
+++ app/src/main/java/com/example/infs3605groupproject/objects/Trail.java
@@ -13,76 +13,79 @@ public class Trail {
 
         ArrayList<Plant> plantList= new ArrayList<>();
         Plant plant = new Plant("1", "Acacia elata", "Mountain Cedar Wattle", "In front of Law Library", "Edible pea flower and pod. Leaves can be used for handwashing when placed in water. Chemicals in the leaves can stun fish. Indigenous hunters use leaves placed in the water in dams to stop the gills from working. Some fish are collected, while the rest are released and left to revive to help maintain the population.", "Species of flowering plant which matures into a tree 30 m high and nearly as wide with a yellow-brown buttressed trunk.",
-                "Coastal Eastern Australia","port_jackson_fig");
+                "Coastal Eastern Australia","a1_fig");
         plantList.add(plant);
         plant = new Plant("2", "Dianella caerulea", "Blue Flax Lily / Blueberry Lily", "Lower Campus", "the sweet fruit can be eaten raw. Leaves are used to make a strong fibre while the purple juice from the berries is used for dying fibres.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW, VIC","acacia_elata_diagram");
+                "QLD, NSW, VIC","atwo_fig");
         plantList.add(plant);
         plant = new Plant("3", "Alpinia caerulea", "Native Ginger", "Lower Campus", "All parts of the plant are edible, both raw and cooked. the roots and the pulp of the berries have a ginger flavour. Leaves can be wrapped around meat and cooked in an earth oven. the large leaves can be used as thatch on shelters, and also to wrap food.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW","acacia_elata_diagram");
+                "QLD, NSW","a3_fig");
         plantList.add(plant);
         plant = new Plant("4", "Brachychiton", "Illawarra Flame Tree", "Lower Campus", "Seeds can be extracted from seed pods, ground and eaten. Seeds are about 18 per cent protein, 25 per cent fat and high in zinc and magnesium.  the sturdy fibre of the inner bark can be made into string, dilly bags and f ishing nets.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW","acacia_elata_diagram");
+                "QLD, NSW","a4_fig");
         plantList.add(plant);
         plant = new Plant("5", "Xanthorrhoea australis", "Grass Tree", "Lower Campus", "nectar comes from the long flowering spikes and the soft bases of young leaves are edible. the stalks from old f lowers and fruits can be used for tinder when making fire. the tall brown stalks can be used to make spear shafts and the soft wood provides a base for a f ire drill when making fire. tough leaves can be used as knives.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "All states","acacia_elata_diagram");
+                "All states","a5_fig");
         plantList.add(plant);
         plant = new Plant("6", "Lomandra longifolia", "Spiny-headed  Mat-rush", "Lower Campus", "Raw leaf bases are a refreshing snack for the bush rambler. tufts of leaves pulled from the clump and the white inner bases can be chewed. they taste like fresh peas. the long slender leaves are excellent for making mats, baskets and food-collecting utensils. Flower heads soaked in water produce a  sweet drink.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW, VIC, TAS, SA","acacia_elata_diagram");
+                "QLD, NSW, VIC, TAS, SA","a6_fig");
         plantList.add(plant);
         plant = new Plant("7", "Syzygium  luehmanni", "Riberry", "Lower Campus", "the white pulp of the fruit makes pleasant eating. the red fruits are often added to fruit salads or used to make jams and jellies.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "North coast QLD, NSW","acacia_elata_diagram");
+                "North coast QLD, NSW","a7_fig");
         plantList.add(plant);
         plant = new Plant("8", "Podocarpus elatus", "Plum Pine/ Brown Pine", "Lower Campus", "the black juicy sweet fruit with its plum-like flavour is highly regarded by Aboriginal people. the seeds can also be eaten but have a resinous flavour. the fruit can be used for savoury or sweet dishes, such as chutneys, jams and desserts.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW","acacia_elata_diagram");
+                "QLD, NSW","a8_fig");
         plantList.add(plant);
         plant = new Plant("9", "Banksia serrata", "Saw Banksia/ Old Man Banksia", "Lower Campus", "Flower spikes steeped in water produce a nectar-sweetened drink. Flowers also attract native animals and birds.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW, VIC, TAS","acacia_elata_diagram");
+                "QLD, NSW, VIC, TAS","a9_fig");
         plantList.add(plant);
         plant = new Plant("10", "Poa labillardieri", "Tussock Grass", "Lower Campus", "Roots were used in cooking.  today this grass is used in gardens in rockeries, near ponds and as a ground cover.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW, VIC, TAS, SA","acacia_elata_diagram");
+                "QLD, NSW, VIC, TAS, SA","a10_fig");
         plantList.add(plant);
         plant = new Plant("11", "Macrozamia  communis", "Burrawang", "Lower Campus", "Seeds can be removed from mature cones and leached in water for a week, with daily water changes, to remove toxins. the pulp of the seeds can then be made into cakes and roasted over embers.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW","acacia_elata_diagram");
+                "QLD, NSW","a11_fig");
         plantList.add(plant);
         plant = new Plant("12", "Ficus coronata", "Sandpaper Fig", "Lower Campus", "the fruits, which are ripe from october to December, can be eaten whole or made into jam. Be sure to remove the hairy skin first. traditionally the rough leaves are used as sandpaper to sharpen tools and weapons and to rub medicine into wounds.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "NT, QLD, NSW, VIC","acacia_elata_diagram");
+                "NT, QLD, NSW, VIC","a12_fig");
         plantList.add(plant);
         plant = new Plant("13", "Dendrobium  speciosum", "Rock Lily", "Lower Campus", "the thickened stems and roots can be chewed or sucked.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "NSW, VIC","acacia_elata_diagram");
+                "NSW, VIC","a13_fig");
         plantList.add(plant);
         plant = new Plant("14", "Cissus antarctica", "Water Vine", "Lower Campus", "Mixed with nectar and water, the fruits are edible but have a watery, acidic taste. Unripe fruits have been traditionally used for stomach complaints while the stems can be used to make harnesses for climbing tall trees.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW","acacia_elata_diagram");
+                "QLD, NSW","a14_fig");
         plantList.add(plant);
         plant = new Plant("15", "Melaleuca  styphelioides", "Prickly-leaved  Tea Tree", "Lower Campus", "the versatile bark can be used for roofing, blankets, rain capes, bandages, plates, caulking for canoes, rafts, baby slings and as torches to smoke away mosquitoes. Leaves can be crushed and sniffed for colds and headaches. An infusion of leaves makes a wash for skin irritants. the nectar-filled flowers can be soaked in water for a drink.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW","acacia_elata_diagram");
+                "QLD, NSW","a15_fig");
         plantList.add(plant);
-        plant = new Plant("16", "Ficus watkinsiana", "Strangling Fig", "Lower Campus", "the succulent figs are a popular and tasty fruit.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "NE QLD, NORTHERN NSW","acacia_elata_diagram");
+        plant = new Plant("16", "Ficus hillii", "Hill's Fig", "Lower Campus", "the succulent figs are a popular and tasty fruit.", "It is generally a very resilient tree, both drought and shade tolerant. It is excellent for hedging or screening or as a pruned specimen to maintain size and shape. Alternatively, it is suitable for ornamental or shade tree applications.",
+                "All states","a16_fig");
         plantList.add(plant);
         plant = new Plant("17", "Ficus rubiginosa", "Port Jackson Fig", "Lower Campus", "the small yellow fruit can be consumed raw or preserved in a ‘cake’ but should only be eaten when ripe, soft and pulpy. the twine made from fibres from the plant is used for dilly bags and the inner bark for fishing nets. the timber can be used to make items for food collection while the sap can be used as a natural latex for wounds.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW","acacia_elata_diagram");
+                "QLD, NSW","a17_fig");
         plantList.add(plant);
         plant = new Plant("18", "Cupaniopsis  anacardioides", "Tuckeroo", "Lower Campus", "the fruit is a pumpkin-shaped  orange-yellow fleshy berry.  It is edible but has a very bitter taste.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "Coastal QLD and NSW","acacia_elata_diagram");
+                "Coastal QLD and NSW","a18_fig");
         plantList.add(plant);
         plant = new Plant("19", "Prostanthera ovalifolia", "Native Mint", "Lower Campus", "Indigenous people have used the leaves to treat colds and headaches. the leaves are crushed and placed on the temple to relieve headaches, or infused in hot water as an inhalant, or used externally. the leaves are antibacterial, antifungal and carminative. native Mint can also be used with the foods that are traditionally accompanied by mint.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "NSW, VIC, TAS","acacia_elata_diagram");
+                "NSW, VIC, TAS","a19_fig");
         plantList.add(plant);
         plant = new Plant("20", "Banksia ericifolia", "Banksia", "Lower Campus", "Flower spikes can be steeped in water and the nectar-sweetened water drunk. Flowers also attract native animals and birds.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "NSW","acacia_elata_diagram");
+                "NSW","a20_fig");
         plantList.add(plant);
         plant = new Plant("21", "Callistemon citrinus", "Crimson Bottlebrush", "Lower Campus", "the leaves are used as a tea substitute and have a delightfully refreshing f lavour. the common use is as a dye for wood. A tan dye is obtained from the f lowers and a cinnamon dye from the leaves.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW, VIC","acacia_elata_diagram");
+                "QLD, NSW, VIC","a21_fig");
         plantList.add(plant);
         plant = new Plant("22", "Melaleuca  quinquenervia", "Paperbark", "Lower Campus", "the leaves can be steeped for sore throats and as a wash, while young leaves are either chewed as a treatment for head colds or brewed in warm water to make a liquid to treat headaches.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "QLD, NSW","acacia_elata_diagram");
+                "QLD, NSW","a22_fig");
         plantList.add(plant);
         plant = new Plant("23", "Doryanthes excelsa", "Gymea Lily", "Lower Campus", "the stems are roasted. they are cut when young and about half a metre tall, with a thickness similar to a person’s arm. the roots can also be roasted and made into a cake to be eaten cold.", "Long-lived wattle-tree, potentially reaching 30m, with a dense conical to medium domed canopy.",
-                "NSW","acacia_elata_diagram");
+                "NSW","a23_fig");
         plantList.add(plant);
         plant = new Plant("24", "Eupomatia laurina", "Bolwarra", "Opposite of UNSW Bookshop", "the sticky sweep pulp coating the seeds is edible.", "Large tree of the family Malvaceae and is famous for the bright red bell-shaped flowers that often cover the whole tree when it is leafless.",
-                "Coastal QLD, NSW and VIC", "illawarra_flame_tree");
+                "Coastal QLD, NSW and VIC", "a24_fig");
+        plantList.add(plant);
+        plant = new Plant("25", "Cabbage Tree Palm", "Cabbage Tree Palm", "Opposite of UNSW Bookshop", "The cabbage tree palm is traditional food for coastal Indigenous", "It did have value for early settlers (ornamental/for shade; copying Indigenous people by eating the hearts; and using the trunks for feeding troughs to cattle).",
+                "QLD, VIC", "a25_fig");
         plantList.add(plant);
         return plantList;
     }
